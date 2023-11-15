c0b24967ad204525763709d763076a420da52986
Carine Guzzi Santos <carineguzzi@usp.br>
2023-11-15 18:00:52 -0300

    Paginas about e index feitas

---
+++ staticpages/views.py
@@ -0,0 +1,12 @@
+# Create your views here.
+from django.shortcuts import render
+
+
+def index(request):
+    context = {}
+    return render(request, 'staticpages/index.html', context)
+
+
+def about(request):
+    context = {}
+    return render(request, 'staticpages/about.html', context)
