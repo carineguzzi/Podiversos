f1a987ba945d6f7ae68ba246296e35c8c17e8385
Carine Guzzi Santos <carineguzzi@usp.br>
2023-11-16 01:45:37 -0300

    atualização da view sem utilizar Django forms

--- post/urls.py
+++ post/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('<int:post_id>/', views.detail_post, name='detail'), # adicione esta linha
     path('create/', views.create_post, name='create'), # adicione esta linha
     path('', views.list_post, name='index'), # adicione esta linha
+    path('update/<int:post_id>/', views.update_post, name='update'),
+    path('delete/<int:post_id>/', views.delete_post, name='delete'),
 ]
--- post/views.py
+++ post/views.py
@@ -1,27 +1,52 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 from .temp_data import post_data
+from django.shortcuts import render, get_object_or_404
 
-def detail_post(request, post_id):
-    post = post_data[post_id - 1]
-    return HttpResponse(
-        f'Detalhes do Podcast {post["name"]} ({post["release_year"]})')
 def list_post(request):
     context = {"post_list": post_data}
     return render(request, 'post/index.html', context)
 
 def detail_post(request, post_id):
-    context = {'post': post_data[post_id - 1]}
+    post = get_object_or_404(Post, pk=post_id)
+    context = {'post': post}
     return render(request, 'post/detail.html', context)
 
 def create_post(request):
     if request.method == 'POST':
-        post_data.append({
-            'name': request.POST['name'],
-            'release_year': request.POST['release_year'],
-            'poster_url': request.POST['poster_url']
-        })
+        post_name = request.POST['name']
+        post_release_year = request.POST['release_year']
+        post_poster_url = request.POST['poster_url']
+        post = Post(name=post_name,
+                      release_year=post_release_year,
+                      poster_url=post_poster_url)
+        post.save()
         return HttpResponseRedirect(
-            reverse('post:detail', args=(len(post_data), )))
+            reverse('post:detail', args=(post.id, )))
     else:
         return render(request, 'post/create.html', {})
+def update_post(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+
+    if request.method == "POST":
+        post.name = request.POST['name']
+        post.release_year = request.POST['release_year']
+        post_poster_url = request.POST['poster_url']
+        post.save()
+        return HttpResponseRedirect(
+            reverse('post:detail', args=(post.id, )))
+
+    context = {'post': post}
+    return render(request, 'post/update.html', context)
+
+
+def delete_post(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+
+    if request.method == "POST":
+        post.delete()
+        return HttpResponseRedirect(reverse('post:index'))
+
+    context = {'post': post}
+    return render(request, 'post/delete.html', context)
